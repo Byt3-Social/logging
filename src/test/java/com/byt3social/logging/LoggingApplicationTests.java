a5bda714049d16bc92c3357ce2e5e534f934aef6
Leandro Pereira <leojgpereira@gmail.com>
2024-06-23 15:17:38 -0400

    Commit inicial

---
+++ src/test/java/com/byt3social/logging/LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byt3social.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
